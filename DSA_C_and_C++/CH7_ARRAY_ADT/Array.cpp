3395f2dc8a7440a04d32b6d256459ca3fc8ec409
TianBaoChiu <seedage57309@gmail.com>
2024-06-13 23:23:46 +0800

    add  binary search algorithm function

--- DSA_C_and_C++/CH7_ARRAY_ADT/Array.cpp
+++ DSA_C_and_C++/CH7_ARRAY_ADT/Array.cpp
@@ -41,6 +41,70 @@ void Insert(struct Array *arr, int index, int x)
     }
 }
 
+int Delete(struct Array *arr, int index)
+{
+    int x = 0;
+    if (index >= 0 && index < arr->length)
+    {
+        x = arr->A[index];
+        for (int i = index; i < arr->length - 1; i++)
+        {
+            arr->A[i] = arr->A[i + 1];
+        }
+        arr->length--;
+        return x;
+    }
+    return 0;
+}
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+int LinearSearch(struct Array *arr, int key)
+{
+    for (int i = 0; i < arr->length; i++)
+    {
+        if (arr->A[i] == key)
+            return i;
+    }
+    return -1;
+}
+
+int BinarySearch(struct Array *arr, int key)
+{
+    int l = 0;
+    int h = arr->length - 1; //--- ex : length = 7 index = 6
+    while (l <= h)
+    {
+        int mid = (l + h) / 2;
+        if (arr->A[mid] == key)
+            return mid;
+        else if (arr->A[mid] > key)
+            h = mid - 1;
+        else
+            l = mid + 1;
+    }
+    return -1;
+}
+
+int RBinarySearch(struct Array *arr, int key, int l, int h)
+{
+    if (l <= h)
+    {
+        int mid = (l + h) / 2;
+        if (arr->A[mid] == key)
+            return mid;
+        else if (arr->A[mid] > key)
+            return RBinarySearch(arr, key, l, mid - 1);
+        else
+            return RBinarySearch(arr, key, l + 1, h);
+    }
+
+    return -1;
+}
+
 int main(void)
 {
     // 初始化一個array
@@ -54,5 +118,18 @@ int main(void)
 
     Insert(&arr, 3, 33);
     Display(arr);
+
+    Delete(&arr, 3);
+    Display(arr);
+
+    int key_index = LinearSearch(&arr, 4);
+    printf("The index is %d\n", key_index);
+
+    int key_index1 = BinarySearch(&arr, 99);
+    printf("The index1 is %d\n", key_index1);
+
+    int key_index2 = RBinarySearch(&arr, 5, 0, arr.length - 1);
+    printf("The index2 is %d\n", key_index2);
+
     return 0;
 }
